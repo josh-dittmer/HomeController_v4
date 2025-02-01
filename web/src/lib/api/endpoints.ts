b25f5881106280d664a5bcb47b19b519bed332e5
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-02-01 08:04:11 +0000

    update endpoints

--- web/src/lib/api/endpoints.ts
+++ web/src/lib/api/endpoints.ts
@@ -3,13 +3,13 @@ export const Endpoints = {
     authApiInternalUrl: `${process.env.NEXT_PUBLIC_AUTH_API_INTERNAL_URL ?? process.env.NEXT_PUBLIC_AUTH_API_PUBLIC_URL!}`,
     authApiPrefix: `${process.env.NEXT_PUBLIC_AUTH_API_PREFIX!}`,
     authApiPublic: `${process.env.NEXT_PUBLIC_AUTH_API_PUBLIC_URL!}${process.env.NEXT_PUBLIC_AUTH_API_PREFIX!}`,
-    authApiInternal: `${process.env.NEXT_PUBLIC_AUTH_API_INTERNAL_URL!}${process.env.NEXT_PUBLIC_AUTH_API_PREFIX!}`,
+    authApiInternal: `${process.env.NEXT_PUBLIC_AUTH_API_INTERNAL_URL ?? process.env.NEXT_PUBLIC_AUTH_API_PUBLIC_URL!}${process.env.NEXT_PUBLIC_AUTH_API_PREFIX!}`,
 
     mainApiPublicUrl: `${process.env.NEXT_PUBLIC_MAIN_API_PUBLIC_URL!}`,
     mainApiInternalUrl: `${process.env.NEXT_PUBLIC_MAIN_API_INTERNAL_URL!}`,
     mainApiPrefix: `${process.env.NEXT_PUBLIC_MAIN_API_PREFIX!}`,
     mainApiPublic: `${process.env.NEXT_PUBLIC_MAIN_API_PUBLIC_URL!}${process.env.NEXT_PUBLIC_MAIN_API_PREFIX!}`,
-    mainApiInternal: `${process.env.NEXT_PUBLIC_AUTH_API_INTERNAL_URL ?? process.env.NEXT_PUBLIC_AUTH_API_PUBLIC_URL!}${process.env.NEXT_PUBLIC_MAIN_API_PREFIX!}`,
+    mainApiInternal: `${process.env.NEXT_PUBLIC_MAIN_API_INTERNAL_URL!}${process.env.NEXT_PUBLIC_MAIN_API_PREFIX!}`,
 
     callbackUrl: `${process.env.NEXT_PUBLIC_SELF_URL!}/callback`
 };
