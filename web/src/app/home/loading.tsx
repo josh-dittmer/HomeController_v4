ded2568c02b5503aa13d38053d55ed0240a4194c
Josh Dittmer <joshua.dittmer@sjsu.edu>
2025-02-10 07:26:02 +0000

    fix loading screen height

--- web/src/app/home/loading.tsx
+++ web/src/app/home/loading.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from "@/components/loading_spinner/loading_spinner";
 
 export default function LoadingPage() {
     return (
-        <div className="flex justify-center items-center h-svh">
+        <div className="flex justify-center items-center h-[calc(100svh-80px)]">
             <LoadingSpinner />
         </div>
     )
